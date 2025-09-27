9b4141e8a6cdc5d2ae0e33353bb101e3d3bfc680
Toginomi <rudyiipilande@gmail.com>
2025-09-28 07:04:21 +0800

    added remove button to cart

--- src/js/cart.js
+++ src/js/cart.js
@@ -1,9 +1,17 @@
-import { getLocalStorage } from "./utils.mjs";
+import { getLocalStorage, setLocalStorage } from "./utils.mjs";
 
 function renderCartContents() {
   const cartItems = getLocalStorage("so-cart");
   const htmlItems = cartItems.map((item) => cartItemTemplate(item));
   document.querySelector(".product-list").innerHTML = htmlItems.join("");
+
+  // Add event listener to all remove buttons
+  document.querySelectorAll(".cart-card__remove").forEach((button) => {
+    button.addEventListener("click", (event) => {
+      const productId = event.target.dataset.id;
+      removeProductFromCart(productId);
+    });
+  });
 }
 
 function cartItemTemplate(item) {
@@ -20,9 +28,17 @@ function cartItemTemplate(item) {
   <p class="cart-card__color">${item.Colors[0].ColorName}</p>
   <p class="cart-card__quantity">qty: 1</p>
   <p class="cart-card__price">$${item.FinalPrice}</p>
+  <span class="cart-card__remove" data-id="${item.Id}">X</span>
 </li>`;
 
   return newItem;
+}
+
+function removeProductFromCart(productId) {
+  let cartItems = getLocalStorage("so-cart") || [];
+  cartItems = cartItems.filter(item => item.Id !== productId);
+  setLocalStorage("so-cart", cartItems);
+  renderCartContents(); // Re-render the cart after removing
 }
 
 renderCartContents();
